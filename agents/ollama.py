93aa054d67f68af988f17910730652da7eacb74e
Alon Ragoler <alan.ragoler@gmail.com>
2025-08-05 21:51:24 -0700

    Added more models to ollama dictionary

--- agents/ollama.py
+++ agents/ollama.py
@@ -4,15 +4,25 @@ from typing import List, Dict
 
 class Ollama(BaseLLM):
     model_dict = {
-        "3.2-llama": "llama3.2:latest"
+        "2-llama": "llama2:latest",
+        "3-llama": "llama3:latest",
+        "3.1-llama": "llama3.1:latest",
+        "3.2-llama": "llama3.2:latest",
+        "3.3-llama": "llama3.3:latest",
+        "gpt-oss": "gpt-oss:latest",
+        "deepseek-r1": "deepseek-r1:latest",
+        "mistral": "mistral:latest",
+        "1-gemma": "gemma:latest",
+        "2-gemma": "gemma2:latest",
+        "3-gemma": "gemma3:latest",
+        "1.5-qwen": "qwen:latest",
+        "2.5-qwen": "qwen2.5:latest",
+        "3-qwen": "qwen3:latest",
     }
-
-    def __init__(self):
-        super().__init__()
 
     @classmethod
     def query(cls, conversation: List[Dict[str, str]], model: str):
-        response =ollama.chat(
+        response = ollama.chat(
             model=cls.get_model_id(model),
             messages=conversation
         )
